25d9de196622400a71c19b8b612cb2b34a484925
Ahmed Ramadan <142809488+ahmd-ramadan@users.noreply.github.com>
2024-11-08 17:07:39 +0200

    Enhance in design

--- Server/server.js
+++ Server/server.js
@@ -1,41 +1,42 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const httpStatusText = require('./utils/httpStatusText');
-const mongoose = require('mongoose');
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const httpStatusText = require("./utils/httpStatusText");
+const mongoose = require("mongoose");
 
 const app = express();
 
 app.use(cors());
-app.use(express.json())
+app.use(express.json());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-require('dotenv').config() 
+require("dotenv").config();
 
-const mongodb_url =  process.env.MONGODB_URL;
+const mongodb_url = process.env.MONGODB_URL;
 mongoose.connect(mongodb_url).then(() => {
-    console.log('mongoose server started')
-})
+  console.log("Database running now ^_^ ...");
+});
 
+const notesRouter = require("./routes/notes-routes");
+app.use("/notes", notesRouter);
 
-const notesRouter = require('./routes/notes-routs')
-app.use('/notes', notesRouter);
-
-app.all('*', (req, res, next) => {
-    return res.status(400).json({status: httpStatusText.ERROR, message: 'This resourse is not available'})
-})
+app.all("*", (req, res, next) => {
+  return res.status(404).json({
+    status: httpStatusText.ERROR,
+    message: "This resourse is not available",
+  });
+});
 
 app.use((error, req, res, next) => {
-    res.status(error.statusCode || 500).json({
-        status: error.statusText || httpStatusText.ERROR,
-        code: error.statusCode || 500,
-        message: error.message || "Error",
-        data: null,
-    })
-})
+  res.status(error.statusCode || 500).json({
+    status: error.statusText || httpStatusText.ERROR,
+    code: error.statusCode || 500,
+    message: error.message || "Error",
+    data: null,
+  });
+});
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-    console.log(`Listening On Port ${PORT}`);
-})
-
+  console.log(`Listening On Port ${PORT} ...`);
+});
